e51da58635ed1779627afb194fe16ce23f96cf87
amrik34 <amrik3412singh@gmail.com>
2026-09-04 11:12:05 +0530

    ResourcePost 1 moving to productions

--- src/components/BrevoNewsletter.tsx
+++ src/components/BrevoNewsletter.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const BrevoNewsletter = () => {
   return (
-    <div className="my-12 brevo-newsletter">
+    <div className="my-12- brevo-newsletter">
       <iframe
         src="/brevo-frame.html"
         className="w-full h-[605px] border-none"
-        title="Newsletter Signup"></iframe>
+        title="Newsletter Signup"
+      ></iframe>
     </div>
   );
 };
